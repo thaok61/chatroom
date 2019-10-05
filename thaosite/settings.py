c2325a9f23c3c16c1715dc1c9c322d9f6101ab66
thao <thao.vd1712@gmail.com>
2019-10-05 15:28:26 +0700

    up

--- thaosite/settings.py
+++ thaosite/settings.py
@@ -124,6 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 ASGI_APPLICATION = "thaosite.routing.application"
 CHANNEL_LAYERS = {
